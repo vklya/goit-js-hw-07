c73dc1dd18145ff233519fc3d9b163ab20b31d02
Viktoria Prysiazhniuk <glushenkovika@gmail.com>
2022-11-20 21:24:44 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,10 +2,8 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const ulGallery = document.querySelector('.gallery');
-const galleryCards = galleryItems.map(galleryItem).join('');
-ulGallery.insertAdjacentHTML('afterbegin', galleryCards);
 
-function galleryItem({ preview, original, description }) {
+const  galleryItem = ({ preview, original, description }) => {
     return `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img
@@ -16,6 +14,9 @@ function galleryItem({ preview, original, description }) {
     </a>
     </li>`
 }
+
+const galleryCards = galleryItems.map(galleryItem).join('');
+ulGallery.insertAdjacentHTML('afterbegin', galleryCards);
 
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
